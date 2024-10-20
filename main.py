1aaf651ccd9b418bb9af5ac7c36740ed74efed89
Mark Beebe <mbeebe08@gmail.com>
2024-10-19 21:17:53 -0700

    add a new current/upcoming week weather api calling agent

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from forecast import get_forecast
 
 def get_one_day_forecast():
     # Call get_user_info with parameters to get back PersonalInfo{lat, long}
-    personal_info = get_user_info(address = "Hood River, Oregon")
+    personal_info = get_user_info(address = "Hood River, Oregon") # TODO: make this a parameter
     # print(personal_info.latitude, personal_info.longitude)
 
     #Use header, lat, long to get relevant weather station metadata & forecast endpoint
@@ -15,7 +15,19 @@ def get_one_day_forecast():
     #Ping forecast endpoint to get forecast as .json
     forecast = get_forecast(forecast_url=forecast_url)
     todays_forecast = forecast[0].detailed_forecast
+
     return todays_forecast
 
+def get_week_forecast():
+    personal_info = get_user_info(address="Hood River, Oregon")
+    forecast_metadata = get_forecast_metadata(personal_info.latitude, personal_info.longitude)
+    forecast = get_forecast(forecast_url=forecast_metadata.forecast_url)
 
+    print(forecast)
 
+    return [{
+        'date': day.day,
+        'forecast': day.detailed_forecast
+    } for day in forecast]
+
+get_week_forecast()
